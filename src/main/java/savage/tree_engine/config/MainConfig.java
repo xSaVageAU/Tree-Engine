5f991b1840d71846734a07a12017fc48d8c029f6
xSaVageAU <jessejd1997@gmail.com>
2025-11-29 01:46:13 +1030

    add some in-line comments to config

--- src/main/java/savage/tree_engine/config/MainConfig.java
+++ src/main/java/savage/tree_engine/config/MainConfig.java
@@ -14,12 +14,11 @@ public class MainConfig {
     public String source_path = "";
     public boolean open_browser_on_start = false;
     
-    // Authentication settings
     public String auth_token = "";
     public boolean auth_enabled = true;
     public boolean regenerate_token_on_restart = true;
     
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
     private static final Path CONFIG_DIR = Path.of("config", "tree_engine");
     private static final Path CONFIG_FILE = CONFIG_DIR.resolve("config.json");
     private static final Path TEXTURES_DIR = CONFIG_DIR.resolve("textures");
@@ -36,8 +35,14 @@ public class MainConfig {
             
             // Load or create config
             if (Files.exists(CONFIG_FILE)) {
-                String json = Files.readString(CONFIG_FILE);
-                instance = GSON.fromJson(json, MainConfig.class);
+                // Use lenient reader to allow comments
+                try (java.io.Reader reader = Files.newBufferedReader(CONFIG_FILE)) {
+                    com.google.gson.stream.JsonReader jsonReader = new com.google.gson.stream.JsonReader(reader);
+                    jsonReader.setLenient(true);
+                    instance = GSON.fromJson(jsonReader, MainConfig.class);
+                }
+                // Save back to ensure format is updated
+                save();
                 TreeEngine.LOGGER.info("Loaded main config");
             } else {
                 instance = new MainConfig();
@@ -55,8 +60,26 @@ public class MainConfig {
     
     public static void save() {
         try {
-            String json = GSON.toJson(instance);
-            Files.writeString(CONFIG_FILE, json);
+            // Manually build JSON string to include comments
+            // Note: We use GSON.toJson() for values to handle escaping properly
+            StringBuilder sb = new StringBuilder();
+            sb.append("{\n");
+            
+            sb.append("  // General Server Settings\n");
+            sb.append("  \"server_port\": ").append(instance.server_port).append(",\n");
+            sb.append("  \"dev_mode_enabled\": ").append(instance.dev_mode_enabled).append(",\n");
+            sb.append("  \"source_path\": ").append(GSON.toJson(instance.source_path)).append(",\n");
+            sb.append("  \"open_browser_on_start\": ").append(instance.open_browser_on_start).append(",\n");
+            
+            sb.append("\n  // Security & Authentication Settings\n");
+            sb.append("  // If auth_token is empty, a new one is generated on webserver startup\n");
+            sb.append("  \"auth_token\": ").append(GSON.toJson(instance.auth_token)).append(",\n");
+            sb.append("  \"auth_enabled\": ").append(instance.auth_enabled).append(",\n");
+            sb.append("  \"regenerate_token_on_restart\": ").append(instance.regenerate_token_on_restart).append("\n");
+            
+            sb.append("}");
+            
+            Files.writeString(CONFIG_FILE, sb.toString());
         } catch (IOException e) {
             TreeEngine.LOGGER.error("Failed to save main config", e);
         }
